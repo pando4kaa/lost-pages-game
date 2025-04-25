b6ddf562710c543e70595dec85fc7d21bab70f0b
Anastasiia Tuhai <tuhaianastasia@gmail.com>
2025-04-25 15:48:59 +0300

    Add new  F1 and T1 waypoints with associated colliders. Now there is a transition between maps

---
+++ Assets/Scripts/MapTransation.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using Cinemachine;
+
+public class MapTransation : MonoBehaviour
+{
+    [SerializeField] PolygonCollider2D mapBounds;
+    CinemachineConfiner confiner;
+    [SerializeField] Direction direction;
+    [SerializeField] float additivePos = 2f;
+
+    enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    private void Awake()
+    {
+        confiner = FindFirstObjectByType<CinemachineConfiner>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Kent"))
+        {
+            confiner.m_BoundingShape2D = mapBounds;
+            UpdateKentPosition(collision.gameObject);
+        }
+    }
+
+    private void UpdateKentPosition(GameObject kent)
+    {
+        Vector3 kentPosition = kent.transform.position;
+        switch (direction)
+        {
+            case Direction.Up:
+                kentPosition.y += additivePos;
+                break;
+            case Direction.Down:
+                kentPosition.y -= additivePos;
+                break;
+            case Direction.Left:
+                kentPosition.x += additivePos;
+                break;
+            case Direction.Right:
+                kentPosition.x -= additivePos;
+                break;
+        }
+        kent.transform.position = kentPosition;
+    }
+}
