824a533b991752c5ae6d677088df520da44bf97f
Anastasiia Tuhai <tuhaianastasia@gmail.com>
2025-04-28 23:37:39 +0300

    worked on the inventory page in the menu.  now there are slots for items. now I can add items and change their location in the inventory. and can also change between 2 items' position

---
+++ Assets/Scripts/Menu/Slot.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public GameObject currentItem;
+    // public int slotNumber;
+
+    // public void OnClick()
+    // {
+        
+    // }
+}
